7f11e7afe09a23333c12f7d633388eae81a78828
BLK LUV [org] <info@blkluv.org>
2025-04-29 03:37:42 -0400

    Update config-overrides.js

--- ohr-frontend/config-overrides.js
+++ ohr-frontend/config-overrides.js
@@ -7,22 +7,28 @@ module.exports = function override(config, env) {
         url: require.resolve('url'),
         assert: require.resolve('assert'),
         buffer: require.resolve('buffer'),
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
     };
+
     config.plugins.push(
         new webpack.ProvidePlugin({
             process: 'process/browser',
             Buffer: ['buffer', 'Buffer'],
         }),
     );
-    const de = dotenv.config().parsed;
+
+    const dotenvResult = dotenv.config();
+    const de = dotenvResult.parsed || {}; // Ensure de is always an object
+
     config.plugins.push(
-        new webpack.DefinePlugin(Object.keys(de).reduce((prev, next) => {
-            prev[`process.env.${next}`] = JSON.stringify(de[next]);
-            return prev;
-        }, {})),
+        new webpack.DefinePlugin(
+            Object.keys(de).reduce((prev, next) => {
+                prev[`process.env.${next}`] = JSON.stringify(de[next]);
+                return prev;
+            }, {})
+        ),
     );
+
     return config;
-    
-}
+};
